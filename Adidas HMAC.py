6a5701a235327a546343fa5fc5e632364da3f1fa
Tim31264 <tim31264.feher@gmail.com>
2017-05-04 13:33:33 +0200

    Add files via upload

---
+++ Adidas HMAC.py
@@ -0,0 +1,594 @@
+import time
+import sys
+import os
+import random
+import readline
+import re
+import syslog
+import binascii
+
+
+print '\033[1;32mThis is a HMAC Generator for Adidas\033[1;m'
+time.sleep(4)
+
+print
+
+print '\033[1;32mIt will create you a HMAC cookie for Adidas\033[1;m'
+time.sleep(2)
+
+print
+
+print '\033[1;32mPlease type 1 for UK\033[1;m'
+time.sleep(0.3)
+
+print '\033[1;32mPlease type 2 for US\033[1;m'
+time.sleep(0.3)
+
+print '\033[1;32mPlease type 3 for RU\033[1;m'
+time.sleep(0.3)
+
+print '\033[1;32mPlease type 4 for AU\033[1;m'
+time.sleep(0.3)
+
+print
+
+
+
+def England():
+    print '\033[1;32mHacking Adidas UK Server...(This can take some time)\033[1;m'
+    time.sleep(23)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mrandomizing variaton...\033[1;m'
+    time.sleep(5)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mAdding IP to the HMAC...\033[1;m'
+    time.sleep(14)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mTesting HMAC...\033[1;m'
+    time.sleep(20)
+
+    print
+
+    print '\033[1;32mFAILED\033[1;m'
+    time.sleep(2)
+
+    print
+
+    print '\033[1;32mRetrying...\033[1;m'
+    time.sleep(24)
+
+    print
+
+    print '\033[1;32mFAILED AGAIN\033[1;m'
+    time.sleep(2)
+
+    print
+
+    print '\033[1;32mStarting again in 3 Seconds\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mStarting in 2 seconds\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mStarting in 1 second\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mStarting...\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mHacking Server...\033[1;m'
+    time.sleep(13)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mAdding IP to the HMAC...\033[1;m'
+    time.sleep(12)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+
+    print
+
+    print '\033[1;32mTesting HMAC...\033[1;m'
+    time.sleep(25)
+
+    print
+
+    print '\033[1;32mSUCCESFULL\033[1;m'
+    time.sleep(5)
+
+    print
+
+    print '\033[1;32mEnjoy your HMAC fam\033[1;m'
+    time.sleep(6)
+
+    print
+
+    print '\033[1;32mAnd do not forget to send me pair of Yeezys\033[1;m'
+    time.sleep(10)
+
+    print
+
+    print '\033[1;32mexp=4hj4848hsf7fes7f5355fs7739524\033[1;m'
+    time.sleep(5)
+
+    print
+
+    print '\033[1;32mCopied HMAC to clipboard\033[1;m'
+    time.sleep(10)
+
+    print
+
+    print '\033[1;32mTrolled fam\033[1;m'
+    time.sleep(4)
+
+    print
+
+    print '\033[1;32mYou can not create a HMAC\033[1;m'
+    time.sleep(3)
+
+    print
+
+    print '\033[1;32mHOLD THIS L FAM\033[1;m'
+    time.sleep(5)
+
+
+def America():
+    print '\033[1;32mHacking Adidas US Server...(This can take some time)\033[1;m'
+    time.sleep(23)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mrandomizing variaton...\033[1;m'
+    time.sleep(5)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mAdding IP to the HMAC...\033[1;m'
+    time.sleep(14)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mTesting HMAC...\033[1;m'
+    time.sleep(20)
+
+    print
+
+    print '\033[1;32mFAILED\033[1;m'
+    time.sleep(2)
+
+    print
+
+    print '\033[1;32mRetrying...\033[1;m'
+    time.sleep(24)
+
+    print
+
+    print '\033[1;32mFAILED AGAIN\033[1;m'
+    time.sleep(4)
+
+    print
+
+    print '\033[1;32mStarting again in 3 Seconds\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mStarting in 2 seconds\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mStarting in 1 second\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mStarting...\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mHacking Server...\033[1;m'
+    time.sleep(13)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mAdding IP to the HMAC...\033[1;m'
+    time.sleep(12)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+
+    print
+
+    print '\033[1;32mTesting HMAC...\033[1;m'
+    time.sleep(25)
+
+    print
+
+    print '\033[1;32mSUCCESFULL\033[1;m'
+    time.sleep(5)
+
+    print
+
+    print '\033[1;32mEnjoy your HMAC fam\033[1;m'
+    time.sleep(3)
+
+    print
+
+    print '\033[1;32mAnd do not forget to send me pair of Yeezys\033[1;m'
+    time.sleep(4)
+
+    print
+
+    print '\033[1;32mexp=4hj4848hsf7fes7f5355fs7739524\033[1;m'
+    time.sleep(5)
+
+    print
+
+    print '\033[1;32mCopied HMAC to clipboard\033[1;m'
+    time.sleep(10)
+
+    print
+
+    print '\033[1;32mTrolled fam\033[1;m'
+    time.sleep(4)
+
+    print
+
+    print '\033[1;32mYou can not create a HMAC\033[1;m'
+    time.sleep(3)
+
+    print
+
+    print '\033[1;32mHOLD THIS L FAM\033[1;m'
+    time.sleep(5)
+
+def Australia():
+    print '\033[1;32mHacking Adidas AU Server...(This can take some time)\033[1;m'
+    time.sleep(23)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mrandomizing variaton...\033[1;m'
+    time.sleep(5)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mAdding IP to the HMAC...\033[1;m'
+    time.sleep(14)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mTesting HMAC...\033[1;m'
+    time.sleep(20)
+
+    print
+
+    print '\033[1;32mFAILED\033[1;m'
+    time.sleep(2)
+
+    print
+
+    print '\033[1;32mRetrying...\033[1;m'
+    time.sleep(24)
+
+    print
+
+    print '\033[1;32mFAILED AGAIN\033[1;m'
+    time.sleep(2)
+
+    print
+
+    print '\033[1;32mStarting again in 3 Seconds\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mStarting in 2 seconds\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mStarting in 1 second\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mStarting...\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mHacking Server...\033[1;m'
+    time.sleep(13)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mAdding IP to the HMAC...\033[1;m'
+    time.sleep(12)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+
+    print
+
+    print '\033[1;32mTesting HMAC...\033[1;m'
+    time.sleep(25)
+
+    print
+
+    print '\033[1;32mSUCCESFULL\033[1;m'
+    time.sleep(5)
+
+    print
+
+    print '\033[1;32mEnjoy your HMAC fam\033[1;m'
+    time.sleep(6)
+
+    print
+
+    print '\033[1;32mAnd do not forget to send me pair of Yeezys\033[1;m'
+    time.sleep(10)
+
+    print
+
+    print '\033[1;32mexp=4hj4848hsf7fes7f5355fs7739524\033[1;m'
+    time.sleep(5)
+
+    print
+
+    print '\033[1;32mCopied HMAC to clipboard\033[1;m'
+    time.sleep(10)
+
+    print
+
+    print '\033[1;32mTrolled fam\033[1;m'
+    time.sleep(4)
+
+    print
+
+    print '\033[1;32mYou can not create a HMAC\033[1;m'
+    time.sleep(3)
+
+    print
+
+    print '\033[1;32mHOLD THIS L FAM\033[1;m'
+    time.sleep(5)
+
+
+def Russia():
+    print '\033[1;32mHacking Adidas RU Server...(This can take some time)\033[1;m'
+    time.sleep(23)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mrandomizing variaton...\033[1;m'
+    time.sleep(5)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mAdding IP to the HMAC...\033[1;m'
+    time.sleep(14)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mTesting HMAC...\033[1;m'
+    time.sleep(20)
+
+    print
+
+    print '\033[1;32mFAILED\033[1;m'
+    time.sleep(2)
+
+    print
+
+    print '\033[1;32mRetrying...\033[1;m'
+    time.sleep(24)
+
+    print
+
+    print '\033[1;32mFAILED AGAIN\033[1;m'
+    time.sleep(2)
+
+    print
+
+    print '\033[1;32mStarting again in 3 Seconds\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mStarting in 2 seconds\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mStarting in 1 second\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mStarting...\033[1;m'
+    time.sleep(1)
+
+    print
+
+    print '\033[1;32mHacking Server...\033[1;m'
+    time.sleep(13)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+    time.sleep(0.5)
+
+    print
+
+    print '\033[1;32mAdding IP to the HMAC...\033[1;m'
+    time.sleep(12)
+
+    print
+
+    print '\033[1;32mDone\033[1;m'
+
+    print
+
+    print '\033[1;32mTesting HMAC...\033[1;m'
+    time.sleep(25)
+
+    print
+
+    print '\033[1;32mSUCCESFULL\033[1;m'
+    time.sleep(5)
+
+    print
+
+    print '\033[1;32mEnjoy your HMAC fam\033[1;m'
+    time.sleep(6)
+
+    print
+
+    print '\033[1;32mAnd do not forget to send me pair of Yeezys\033[1;m'
+    time.sleep(10)
+
+    print
+
+    print '\033[1;32mexp=4hj4848hsf7fes7f5355fs7739524\033[1;m'
+    time.sleep(5)
+
+    print
+
+    print '\033[1;32mCopied HMAC to clipboard\033[1;m'
+    time.sleep(10)
+
+    print
+
+    print '\033[1;32mTrolled fam\033[1;m'
+    time.sleep(4)
+
+    print
+
+    print '\033[1;32mYou can not create a HMAC\033[1;m'
+    time.sleep(3)
+
+    print
+
+    print '\033[1;32mHOLD THIS L FAM\033[1;m'
+    time.sleep(5)
+
+
+
+
+
+
+if __name__ == '__main__':
+    print '\033[1;32m1 UK\033[1;m'
+    print '\033[1;32m2 US\033[1;m'
+    print '\033[1;32m3 RU\033[1;m'
+    print '\033[1;32m4 AU\033[1;m'
+    time.sleep(3)
+
+    a = input (":")
+    if a == "1" or a == 1:
+        England()
+    elif a == "2" or a == 2:
+        America()
+    elif a == "3" or a == 3:
+        Russia()
+    elif a == "4" or a == 4:
+        Australia()
+
+    else:
+        pass
